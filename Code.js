9c24e8f2861f8e7161c16b7e94def592009a68fb
Arpan <himeshjhanjhariya@gmail.com>
2023-12-20 12:13:56 +0530

    completed the project buy adding proper css to it

--- Code.js
+++ Code.js
@@ -1,47 +1,166 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtn = document.querySelectorAll(".choiceBtn");
-let player;
-let computer;
-let result;
+// const playerText = document.querySelector("#playerText");
+// const computerText = document.querySelector("#computerText");
+// const resultText = document.querySelector("#resultText");
+// const choiceBtn = document.querySelectorAll(".choiceBtn");
+// let player;
+// let computer;
+// let result;
 
-choiceBtn.forEach(button => button.addEventListener("click", () => {
-    player = button.textContent;
-    computerTurn();
-    playerText.textContent = `player : ${player}`;
-    computerText.textContent = `computer : ${computer}`;
-    resultText. textContent = checkWinner();
+// choiceBtn.forEach(button => button.addEventListener("click", () => {
+//     player = button.textContent;
+//     computerTurn();
+//     playerText.textContent = `player : ${player}`;
+//     computerText.textContent = `computer : ${computer}`;
+//     resultText. textContent = checkWinner();
 
-}));
+// }));
 
-function computerTurn() {
-    const random = Math.floor(Math.random() * 3) +1;
+// function computerTurn() {
+//     const random = Math.floor(Math.random() * 3) +1;
 
-    switch (random){
-        case 1:
-            computer = "ROCK";
-            break;
-        case 2:
-            computer = "PAPER";
-            break;  
-        case 3:
-            computer = "SCISSORS";
-            break; 
+//     switch (random){
+//         case 1:
+//             computer = "ROCK";
+//             break;
+//         case 2:
+//             computer = "PAPER";
+//             break;  
+//         case 3:
+//             computer = "SCISSORS";
+//             break; 
 
+//     }
+// }
+// function checkWinner() {
+//     if (computer == player){
+//         return "Draw!";
+//     }
+//     else if (computer == "ROCK") {
+//        return (player == "PAPER") ? "You win!" : "You lose!";
+//     }
+//     else if (computer == "PAPER") {
+//         return (player == "SCISSORS") ? "You win!" : "You lose!";
+//      }
+//      else if (computer == "SCISSORS") {
+//         return (player == "ROCK") ? "You win!" : "You lose!";
+//      }
+// }
+
+// Get all necessary DOM nodes
+const images = Array.from(document.querySelectorAll('.card-image'));
+const message = document.querySelector('.message');
+const scorePlayer = document.querySelector('.player-score');
+const scoreComputer = document.querySelector('.computer-score');
+const selectionPlayer = document.querySelector('.player');
+const selectionComputer = document.querySelector('.computer');
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Start Game when user clicks on an image
+images.forEach((image) =>
+  image.addEventListener('click', () => {
+    if (playerScore >= 5 || computerScore >= 5) {
+      return;
     }
+    game(image.dataset.image);
+  })
+);
+
+/* Game Logic */
+
+function getComputerSelection() {
+  let computerNumber = random(3);
+  let computerGuess = '';
+
+  switch (computerNumber) {
+    case 1:
+      computerGuess = 'Rock';
+      break;
+    case 2:
+      computerGuess = 'Paper';
+      break;
+    case 3:
+      computerGuess = 'Scissors';
+      break;
+    default:
+      break;
+  }
+
+  return computerGuess;
 }
-function checkWinner() {
-    if (computer == player){
-        return "Draw!";
+
+function playRound(playerSelection, computerSelection) {
+  let log = '';
+
+  if (playerSelection === 'Rock') {
+    if (computerSelection === 'Paper') {
+      log = 'You Lose! Paper beats Rock';
+    } else if (computerSelection === 'Scissors') {
+      log = 'You Win! Rock beats Scissors';
+    } else {
+      log = "It's a tie";
     }
-    else if (computer == "ROCK") {
-       return (player == "PAPER") ? "You win!" : "You lose!";
+  } else if (playerSelection === 'Paper') {
+    if (computerSelection === 'Scissors') {
+      log = 'You Lose! Scissors beats Paper';
+    } else if (computerSelection === 'Rock') {
+      log = 'You Win! Paper beats Rock';
+    } else {
+      log = "It's a tie";
     }
-    else if (computer == "PAPER") {
-        return (player == "SCISSORS") ? "You win!" : "You lose!";
-     }
-     else if (computer == "SCISSORS") {
-        return (player == "ROCK") ? "You win!" : "You lose!";
-     }
+  } else if (playerSelection === 'Scissors') {
+    if (computerSelection === 'Rock') {
+      log = 'You Lose! Rock beats Scissors';
+    } else if (computerSelection === 'Paper') {
+      log = 'You Win! Scissors beats Paper';
+    } else {
+      log = "It's a tie";
+    }
+  }
+
+  return log;
+}
+
+function createParagWithText(text) {
+  const p = document.createElement('p');
+  p.textContent = text;
+
+  return p;
+}
+
+function game(playerSelect) {
+  let playerSelection = capitalize(playerSelect);
+  let computerSelection = getComputerSelection();
+
+  let roundResult = playRound(playerSelection, computerSelection);
+
+  if (roundResult.search('You Win!') > -1) {
+    playerScore++;
+  } else if (roundResult.search('You Lose!') > -1) {
+    computerScore++;
+  }
+
+  scorePlayer.textContent = playerScore;
+  scoreComputer.textContent = computerScore;
+  message.textContent = roundResult;
+  selectionPlayer.appendChild(createParagWithText(playerSelection));
+  selectionComputer.appendChild(createParagWithText(computerSelection));
+
+  if (playerScore >= 5 && computerScore < 5) {
+    message.textContent = 'Game Over. You Win!';
+  } else if (playerScore < 5 && computerScore >= 5) {
+    message.textContent = 'Game Over. You Lose!';
+  }
+}
+
+/* Helper Functions */
+function random(number) {
+  return Math.floor(Math.random() * number + 1);
+}
+
+function capitalize(string) {
+  return (
+    string.toLowerCase().charAt(0).toUpperCase() + string.toLowerCase().slice(1)
+  );
 }
